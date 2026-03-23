34ed1865002a7cd9442b43db1bc0b53c840b0f0d
JagerMeihter <kroec@bk.ru>
2026-03-23 15:21:48 +0300

    Добавьте файлы проекта.

---
+++ TechSupportApp/Models/Request.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupportApp.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string ClientName { get; set; }
+        public string ClientPhone { get; set; }
+        public int EquipmentTypeId { get; set; }
+        public EquipmentType EquipmentType { get; set; }
+        public string Description { get; set; }
+        public int PriorityId { get; set; }
+        public Priority Priority { get; set; }
+        public int StatusId { get; set; }
+        public RequestStatus Status { get; set; }
+        public int? AssignedToUserId { get; set; }
+        public User AssignedToUser { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public string RepairComment { get; set; }
+    }
+}
---
+++ TechSupportApp/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupportApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public ICollection<Request> AssignedRequests { get; set; }
+    }
+}
